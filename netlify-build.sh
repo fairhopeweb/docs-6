e3a77afc63078f65a112b43817c33ab5b7f14a77
Jakob Borg <jakob@kastelo.net>
2022-01-12 13:04:59 +0100

    wip

--- netlify-build.sh
+++ netlify-build.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 mkdir -p _build/html
 
+curl -LO https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+rclone=rclone-*/rclone
+
 export RCLONE_CONFIG_AMS3_TYPE=s3
 export RCLONE_CONFIG_AMS3_ENDPOINT=ams3.digitaloceanspaces.com
-rclone sync -v ams3:syncthing-docs/ _build/html/
+$rclone sync -v ams3:syncthing-docs/ _build/html/
 
 make html
 make man
