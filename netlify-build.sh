9db561c8e7386926e8dfb191cad09cf84c8bd522
Jakob Borg <jakob@kastelo.net>
2022-01-12 12:54:44 +0100

    see if rclone is available

--- netlify-build.sh
+++ netlify-build.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 mkdir -p _build/html
 
+export RCLONE_CONFIG_AMS3_TYPE=s3
+export RCLONE_CONFIG_AMS3_ENDPOINT=ams3.digitaloceanspaces.com
+rclone sync -v ams3:syncthing-docs/ _build/html/
+
 make html
 make man
 mv _build/man _build/html
